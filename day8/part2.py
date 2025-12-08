5c707715e40de7c2befa724c68cf393f171d580e
The Batman <hectorscout@hotmail.com>
2025-12-07 23:43:50 -0700

    add day8 solutions

---
+++ day8/part2.py
@@ -0,0 +1,54 @@
+import math
+import os
+# inputFile = "sample.txt"
+inputFile = "input1.txt"
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), inputFile), "r") as file:
+   content = file.read()
+
+points = [tuple(map(int, x.split(","))) for x in content.split('\n')]
+pointCount = len(points)
+
+def getDistance(point1, point2):
+    x1, y1, z1 = point1
+    x2, y2, z2 = point2
+    return math.sqrt(((x1 - x2) **2) + ((y1 - y2) **2) + ((z1 - z2) **2))
+
+distances = []
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        distances.append((points[i], points[j], getDistance(points[i], points[j])))
+
+def distanceSorter(e):
+    return e[2]
+
+distances.sort(key=distanceSorter)
+
+pointGraph = {}
+
+def getGraphNodeCount(point, visitedPoints):  
+    if point in visitedPoints:
+        return 0
+    visitedPoints[point] = True
+    count = 1
+    if not point in pointGraph:
+        return count
+    for edge in pointGraph[point]:
+        count += getGraphNodeCount(edge, visitedPoints)
+    return count
+
+def checkGraph():
+    return pointCount == getGraphNodeCount(points[0], {})
+
+def addGraphEdge(point1, point2):
+    if not point1 in pointGraph:
+        pointGraph[point1] = []
+    if not point2 in pointGraph:
+        pointGraph[point2] = []
+    pointGraph[point1].append(point2)
+    pointGraph[point2].append(point1)
+
+for point1, point2, distance in distances:
+    addGraphEdge(point1, point2)
+    if checkGraph():
+        print(point1[0] * point2[0])
+        break
